09fb65315854a4d09b6a4e695a87505313e75932
jashlj2008-cyber <jashlj2008@gmail.com>
2026-03-28 12:14:48 +0530

    Add files via upload

---
+++ 1ADDISIO.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+//addision of two nos
+void main()
+{
+	int a,b,ans;
+	clrscr();
+	printf("\nEnter value :- ");
+	scanf("%d",&a);
+	printf("\nEnter value :- ");
+	scanf("%d",&b);
+
+	ans=a+b;
+	printf("\nans is %d",ans);
+	getch();
+}
+
